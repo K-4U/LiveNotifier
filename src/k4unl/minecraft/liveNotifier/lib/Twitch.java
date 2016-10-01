46d93755cd58ac0c0faa37a5be115f77c3bcbdd4
Koen Beckers <koen@k-4u.nl>
2016-10-02 00:26:04 +0200

    Made sure we send a client ID towards the server too.

--- src/k4unl/minecraft/liveNotifier/lib/Twitch.java
+++ src/k4unl/minecraft/liveNotifier/lib/Twitch.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Type;
 import java.net.MalformedURLException;
 import java.net.SocketException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 /**
@@ -17,9 +18,30 @@ import java.util.Map;
  */
 public class Twitch {
     private static final String twitchAPI = "https://api.twitch.tv/kraken/";
+    private static String twitchClientId = "";
     private static Gson nGson = new Gson();
     private static Type typeMapStringObject = new TypeToken<Map<String, Object>>() {}.getType();
-
+    
+    static {
+        //Quickly fetch the client ID from the website of K-4U:
+        Log.info("Fetching twitch Client ID from K-4U.eu");
+        try{
+            URL toFetch = new URL("http://www.k-4u.eu/twitchid.txt");
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(toFetch.openStream()));
+            
+            String line;
+            while((line = in.readLine()) != null){
+                twitchClientId += line;
+            }
+            Log.info("Fetched Twitch ID: " + twitchClientId);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     public static Map<String, Object> getChannelData(String channelName){
         String json = fetchJson("channels/" + channelName);
 
@@ -33,9 +55,11 @@ public class Twitch {
 
             URL toFetch = new URL(twitchAPI + url);
             //Log.info("Fetching " + toFetch.toString());
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(toFetch.openStream()));
-
+            URLConnection urlConnection = toFetch.openConnection();
+            urlConnection.addRequestProperty("Client-ID", twitchClientId);
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            
             String line;
             while((line = in.readLine()) != null){
                 jsonEnd += line;
